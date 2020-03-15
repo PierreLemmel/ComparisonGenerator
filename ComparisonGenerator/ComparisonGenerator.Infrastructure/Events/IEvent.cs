f6e2394f0b49ee5e5fbd8988b31f6b45268149cc
Pierre Lemmel <pierre.lemmel@gmail.com>
2020-03-15 20:38:02 +0100

    Bases of event system

---
+++ ComparisonGenerator/ComparisonGenerator.Infrastructure/Events/IEvent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ComparisonGenerator.Infrastructure.Events
+{
+    public interface IEvent
+    {
+        
+    }
+}
---
+++ ComparisonGenerator/ComparisonGenerator.Infrastructure/Events/IEventStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisonGenerator.Infrastructure.Events
+{
+    public interface IEventStore
+    {
+        Task HandleEvent<TEvent>(TEvent evt);
+    }
+}
